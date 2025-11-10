232aa188de50ccb8f67feddd7e7954ef9532e900
CaptainAlola <matvey-fox@mail.ru>
2025-11-10 13:44:01 +0700

    InventoryUpdate

--- Assets/Scipts/Inventory&Shop/InventoryManager.cs
+++ Assets/Scipts/Inventory&Shop/InventoryManager.cs
@@ -4,8 +4,17 @@ using static UnityEditor.Timeline.Actions.MenuPriority;
 
 public class InventoryManager : MonoBehaviour
 {
+    public InventorySlot[] itemSlots;
     public int gold;
     public TMP_Text goldText;
+
+    private void Start()
+    {
+        foreach (var slot in itemSlots) 
+        {
+            slot.UpdateUI();
+        }
+    }
     private void OnEnable()
     {
         Loot.OnItemLooted += AddItem;
@@ -27,7 +36,18 @@ public class InventoryManager : MonoBehaviour
         }
         else 
         {
-         //   UpdateUI();
+            foreach (var slot in itemSlots) 
+            {
+                if (slot.itemSO == null) 
+                {
+                    slot.itemSO = itemSO;
+                    slot.quantity = quantity;
+                    slot.UpdateUI();
+                    return;
+                }
+                
+            }
+           
         }
     }
 
